832004f63b69a9bb7c556e2aa8ac425bc512a273
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-22 18:00:23 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm wget
+pacman -Syu --noconfirm wget curl coreutils
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
