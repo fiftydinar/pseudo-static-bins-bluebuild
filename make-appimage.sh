d956e73f74d54b57aa10ad6ff668c6d892b7e6bd
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-22 17:06:19 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,9 +3,12 @@
 set -eu
 
 ARCH=$(uname -m)
+APPNAME=wget
 VERSION=$(pacman -Q wget | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 
 # Deploy dependencies
 /tmp/sharun-aio --with-wrappe /usr/bin/wget --dst-dir ./dist
+
+mv -v ./dist/$APPNAME ./dist/$APPNAME-$VERSION-wrappe
